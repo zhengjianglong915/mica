2ac17b13efd7110a7096e73f5584db061195a566
zhengjianglong <zhengjianglong@baidu.com>
2019-02-01 00:27:32 +0800

    add mica-server

---
+++ doc/mica.sql
@@ -0,0 +1,3 @@
+# --- create database for mica server
+create database mica;
+
---
+++ mica-server/src/main/java/cn/along/mica/MicaServerApplication.java
@@ -0,0 +1,14 @@
+package cn.along.mica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicaServerApplication.class, args);
+	}
+
+}
+
